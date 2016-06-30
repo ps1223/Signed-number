1d6f8602b5f0678052b58fa198e9ba9493477fad
ps1223 <gpsarathy153@gmail.com>
2016-06-30 21:36:58 +0530

    Create PrimeSeries.java

---
+++ PrimeSeries.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class PrimeSeries {
+
+    public static void main(String arg[]) {
+
+        int a, b, i;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        for (int n = a + 1; n < b; n++) {
+            for (i = 2; i < n / 2; i++) {
+
+                if (n % i == 0) {
+                    break;
+                }
+
+            }
+            if (i == n / 2) {
+                System.out.println(n);
+            }
+        }
+    }
+}
