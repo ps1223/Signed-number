dc5413367ef74822486da6d6e4fab4f3d156bf12
ps1223 <gpsarathy153@gmail.com>
2016-07-01 10:02:42 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PS
+ */
+public class Index {
+    public static void main(String arg[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == i)
+                System.out.println(arr[i]);
+        }
+    }
+}
