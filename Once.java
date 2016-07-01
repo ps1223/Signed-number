f39d2dd8fc0540085d51343a7b0d55106db52a18
ps1223 <gpsarathy153@gmail.com>
2016-07-01 10:20:00 +0530

    Create Once.java

---
+++ Once.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Once {
+
+    public static void main(String arg[]){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int num = arr[0];
+        int count;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            count = 0;
+            for(int j = 0; j < n; j++) {
+                if(arr[i] == arr[j])
+                    count++;
+            }
+            if(count == 1) {
+                num = arr[i];
+                break;
+            }
+        }
+        System.out.print(num);
+    }
+}
